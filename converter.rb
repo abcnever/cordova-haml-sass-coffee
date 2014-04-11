9d6873a44bbafb71fb085eed9adbad5fee588e1e
Edward Zhang <edwardforcpu@gmail.com>
2014-04-10 20:19:48 -0400

    ops

--- converter.rb
+++ converter.rb
@@ -1,5 +1,5 @@
 watch 'src/haml/.*\.haml' do |md| system('thor convert:haml') end
 watch 'src/haml/.*\.rb' do |md| system('thor convert:haml') end
-watch 'src/sass/.*\.scss' do |md| system(`thor convert:sass`) end
-watch 'src/sass/.*\.sass' do |md| system(`thor convert:sass`) end
-watch 'src/coffeescript/.*\.coffee' do |md| system(`thor convert:coffee`) end
+watch 'src/sass/.*\.scss' do |md| system('thor convert:sass') end
+watch 'src/sass/.*\.sass' do |md| system('thor convert:sass') end
+watch 'src/coffeescript/.*\.coffee' do |md| system('thor convert:coffee') end
