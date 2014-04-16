a06a49f7875aa861402fbdf7d9df0eebe5a99aa0
Edward Zhang <edwardforcpu@gmail.com>
2014-04-16 02:32:43 -0400

    now monitor all files under haml folder

--- converter.rb
+++ converter.rb
@@ -1,5 +1,5 @@
-watch 'src/haml/.*\.haml' do |md| system('thor convert:haml') end
-watch 'src/haml/.*\.rb' do |md| system('thor convert:haml') end
-watch 'src/sass/.*\.scss' do |md| system('thor convert:sass') end
-watch 'src/sass/.*\.sass' do |md| system('thor convert:sass') end
-watch 'src/coffeescript/.*\.coffee' do |md| system('thor convert:coffee') end
+watch 'src/haml/.*\.haml' do |md| system("thor convert:haml #{md[0]}") end
+watch 'src/haml/.*\.rb' do |md| system("thor convert:haml #{md[0]}") end
+watch 'src/sass/.*\.scss' do |md| system("thor convert:sass #{md[0]}") end
+watch 'src/sass/.*\.sass' do |md| system("thor convert:sass #{md[0]}") end
+watch 'src/coffeescript/.*\.coffee' do |md| system("thor convert:coffee #{md[0]}") end
--- thor.rb
+++ thor.rb
@@ -4,30 +4,45 @@ class Convert < Thor
   Dir.chdir(ROOT_DIR)
 
   desc "haml", "converts and puts haml in www"
-  def haml
-    `haml -r ./src/haml/helpers.rb ./src/haml/index.html.haml ./www/index.html`
+  def haml(file)
+    file_basename = basename(file)
+
+    if file_basename.include?('.html') && file_basename.include?('.haml')
+      o_file = file_basename.gsub /.haml/, ''
+    elsif !file_basename.include?('.html') && file_basename.include?('.haml')
+      o_file = file_basename.gsub /.haml/, '.html'
+    else
+      return
+    end
+
+    `haml -I ./src/haml/helpers/ ./#{file} ./www/#{o_file}`
   end
 
   desc "sass", "converts and puts sass in www"
-  def sass
-    `sass --update ./src/sass/:./www/css/`
+  def sass(file)
+    file_basename = basename(file)
+
+    if (file_basename.include?('.sass') || file_basename.include?('.scss')) && file_basename.include?('.css')
+      o_file = file_basename.gsub /.sass/, ''
+      o_file = file_basename.gsub /.scss/, ''
+    elsif (file_basename.include?('.sass') || file_basename.include?('.scss')) && !file_basename.include?('.css')
+      o_file = file_basename.gsub /.sass/, '.css'
+      o_file = file_basename.gsub /.scss/, '.css'
+    else
+      return
+    end
+
+    `sass ./#{file}:./www/css/#{o_file}`
   end
 
   desc "coffee", "converts and puts coffeescript in www"
-  def coffee
-    `coffee -o ./www/js -c ./src/coffeescript/`
+  def coffee(file)
+    `coffee -o ./www/js/ -c ./#{file}`
   end
 
-  desc "all", "Convert haml, sass and coffee"
-  def all
-    invoke :haml
-    invoke :sass
-    invoke :coffee
-  end
+  private
 
-  # desc "watch", "Start watchr to convert haml, sass and coffee source as it is modified"
-  # def watch
-  #   invoke :all
-  #   system "cd #{ROOT_DIR} && watchr converter.rb"
-  # end
+  def basename(filename)
+    Pathname.new(filename).basename.to_s
+  end
 end
